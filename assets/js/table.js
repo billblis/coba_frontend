582b9462c9719f3cc053616058bb7264b001a819
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-12 18:49:05 +0700

    update

--- assets/js/table.js
+++ assets/js/table.js
@@ -2,26 +2,63 @@ export const formPemasukan = `
 <tbody
                     class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
                   >
-<tr class="h-18 text-gray-700 dark:text-gray-400">
-    <th class="whitespace-nowrap px-4 text-sm font-medium text-coolGray-800 text-left">#TANGGAL_MASUK#</th>
-    <th class="whitespace-nowrap px-4 text-sm font-medium text-coolGray-800 text-left">
-    <span
-    class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
-  >
-  Rp. #JUMLAH_MASUK#
-  </span>
-  </th>
-    <th class="whitespace-nowrap px-4 text-sm font-medium text-coolGray-800 text-left">#SUMBER#</th>
-    <th class="whitespace-nowrap px-4 text-sm font-medium text-coolGray-500 text-left">#DESKRIPSI#</th>
-    <th class="whitespace-nowrap pr-4 text-sm font-medium text-coolGray-800">
-<a type="button" href="edit_pemasukan.html?_id=#IDEDIT#" data-pemasukan-id="#IDHAPUS#"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
-</a>
-<button type="button" onclick="deletePemasukan('#DELETE#')"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-</button>
-</th>
-</tr>
-<tbody>
+
+                    <tr class="text-gray-700 dark:text-gray-400">
+                      <td class="px-4 py-3 text-sm">
+                      #TANGGAL_MASUK# 
+                      </td>
+                      <td class="px-4 py-3 text-sm">
+                       #SUMBER#
+                      </td>
+                      <td class="px-4 py-3 text-xs">
+                        <span
+                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
+                        >
+                        #JUMLAH_MASUK#
+                        </span>
+                      </td>
+                      <td class="px-4 py-3">
+                        <div class="flex items-center space-x-4 text-sm">
+                          <a type="button" href="edit_pemasukan.html?_id=#IDEDIT#" data-pemasukan-id="#IDHAPUS#">
+                          <button
+                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                            aria-label="Edit"
+                          >
+                            <svg
+                              class="w-5 h-5"
+                              aria-hidden="true"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                              ></path>
+                            </svg>
+                          </button>
+                        </a>
+                          <button type="button" onclick="deletePemasukan('#DELETE#')"
+                          class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                          aria-label="Delete"
+                        >
+                          <svg
+                            class="w-5 h-5" 
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                          >
+                            <path fill-rule="evenodd"
+                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                              clip-rule="evenodd"
+                            ></path>
+                          </svg>
+                        </button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
 `;
+
+
 
 export const formPengeluaran = `
 <tbody
