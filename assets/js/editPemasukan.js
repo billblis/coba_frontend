88a3892d10d0cd2807e75693ac1e1cce0e211104
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-12 15:14:54 +0700

    update

--- assets/js/editPemasukan.js
+++ assets/js/editPemasukan.js
@@ -1,24 +1,29 @@
+
 export const isiData = (results) => {
-    const dataTodo = [
-      {id: "tanggal_masuk", path: "data.0.tanggal_masuk"},
-      {id: "jumlah_masuk", path: "data.0.jumlah_masuk"},
-      {id: "sumber", path: "data.0.sumber"},
-      {id: "deskripsi", path: "data.0.deskripi"},
+    const dataPemasukan = [
+      { id: "tanggal_masuk", path: "data.0.tanggal_masuk" },
+      { id: "jumlah_masuk", path: "data.0.jumlah_masuk" },
+      { id: "sumber", path: "data.0.sumber" },
+      { id: "deskripsi", path: "data.0.deskripsi" },
     ];
   
-  
-    dataTodo.forEach(({ id, path, index, property }) => {
+    dataPemasukan.forEach(({ id, path, index, property }) => {
       const inputElement = document.getElementById(id);
       const value = getNestedValue(results, path, index, property);
-      inputElement.value = value;
+  
+      // Check if the property is 'jumlah_masuk' and if its type is a number
+      if (property === "jumlah_masuk" && typeof value === "number") {
+        inputElement.value = value.toString(); // Convert to string if necessary
+      } else {
+        inputElement.value = value;
+      }
     });
-  }
+  };
   
   const getNestedValue = (obj, path, index, property) => {
     const value = path
       .split(".")
       .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
-  
   
     if (
       Array.isArray(value) &&
@@ -30,3 +35,37 @@ export const isiData = (results) => {
   
     return value;
   };
+  
+
+// export const isiData = (results) => {
+//     const dataPemasukan = [
+//       {id: "tanggal_masuk", path: "data.0.tanggal_masuk"},
+//       {id: "jumlah_masuk", path: "data.0.jumlah_masuk"},
+//       {id: "sumber", path: "data.0.sumber"},
+//       {id: "deskripsi", path: "data.0.deskripsi"},
+//     ];
+  
+  
+//     dataPemasukan.forEach(({ id, path, index, property }) => {
+//       const inputElement = document.getElementById(id);
+//       const value = getNestedValue(results, path, index, property);
+//       inputElement.value = value;
+//     });
+//   }
+  
+//   const getNestedValue = (obj, path, index, property) => {
+//     const value = path
+//       .split(".")
+//       .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
+  
+  
+//     if (
+//       Array.isArray(value) &&
+//       value.length > index &&
+//       value[index].hasOwnProperty(property)
+//     ) {
+//       return value[index][property];
+//     }
+  
+//     return value;
+//   };
