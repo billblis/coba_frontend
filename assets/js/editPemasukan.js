bba1e10ac4fda39cc619499a25641ff244498e82
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-12 15:34:11 +0700

    update

--- assets/js/editPemasukan.js
+++ assets/js/editPemasukan.js
@@ -1,31 +1,22 @@
 export const isiData = (results) => {
     const dataPemasukan = [
       { id: "tanggal_masuk", path: "data.0.tanggal_masuk" },
-      { id: "jumlah_masuk", path: "data.0.jumlah_masuk", type: "integer" },
+      { id: "jumlah_masuk", path: "data.0.jumlah_masuk" },
       { id: "sumber", path: "data.0.sumber" },
       { id: "deskripsi", path: "data.0.deskripsi" },
     ];
   
-    dataPemasukan.forEach(({ id, path, type, index, property }) => {
+    dataPemasukan.forEach(({ id, path, index, property }) => {
       const inputElement = document.getElementById(id);
       const value = getNestedValue(results, path, index, property);
   
-      // Check the data type and convert if necessary
-      const processedValue = processValue(value, type);
-  
-      inputElement.value = processedValue;
+      // Convert 'jumlah_masuk' value to integer
+      if (id === "jumlah_masuk") {
+        inputElement.value = parseInt(value, 10);
+      } else {
+        inputElement.value = value;
+      }
     });
-  };
-  
-  const processValue = (value, type) => {
-    if (type === "integer") {
-      // Convert to integer
-      return parseInt(value, 10) || 0; // Use parseInt with base 10, fallback to 0 if conversion fails
-    }
-  
-    // Add additional type checks and conversions for other types if needed
-  
-    return value;
   };
   
   const getNestedValue = (obj, path, index, property) => {
@@ -43,6 +34,7 @@ export const isiData = (results) => {
   
     return value;
   };
+  
   
   
 
