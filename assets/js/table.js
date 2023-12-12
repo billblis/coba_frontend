fd1ce753a3def344e7338274a2702d321e082b38
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-12 19:06:09 +0700

    update

--- assets/js/table.js
+++ assets/js/table.js
@@ -8,21 +8,17 @@ export const formPemasukan = `
                       #TANGGAL_MASUK# 
                       </td>
                       <td class="px-4 py-3 text-xs">
-                      Rp. #JUMLAH_MASUK#
+                      <span
+                      class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
+                    >
+                    Rp. #JUMLAH_MASUK#
+                    </span>
                       </td>
                       <td class="px-4 py-3 text-sm">
-                        <span
-                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
-                        >
-                        #SUMBER#
-                        </span>
+                       #SUMBER#
                       </td>
                       <td class="px-4 py-3 text-sm">
-                        <span
-                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
-                        >
                         #DESKRIPSI#
-                        </span>
                       </td>
                       <td class="px-4 py-3">
                         <div class="flex items-center space-x-4 text-sm">
