b43810df7734022812500c095186664f5c7b1997
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-12 14:49:04 +0700

    update

---
+++ assets/js/editPemasukan.js
@@ -0,0 +1,32 @@
+export const isiData = (results) => {
+    const dataTodo = [
+      {id: "tanggal_masuk", path: "data.0.tanggal_masuk"},
+      {id: "jumlah_masuk", path: "data.0.jumlah_masuk"},
+      {id: "sumber", path: "data.0.sumber"},
+      {id: "deskripsi", path: "data.0.deskripi"},
+    ];
+  
+  
+    dataTodo.forEach(({ id, path, index, property }) => {
+      const inputElement = document.getElementById(id);
+      const value = getNestedValue(results, path, index, property);
+      inputElement.value = value;
+    });
+  }
+  
+  const getNestedValue = (obj, path, index, property) => {
+    const value = path
+      .split(".")
+      .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
+  
+  
+    if (
+      Array.isArray(value) &&
+      value.length > index &&
+      value[index].hasOwnProperty(property)
+    ) {
+      return value[index][property];
+    }
+  
+    return value;
+  };
---
+++ assets/js/updatePemasukan.js
@@ -0,0 +1,65 @@
+import { getValue } from "https://jscroot.github.io/element/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const putData = (target_url, data, responseFunction) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", getCookie("Authorization"));
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: JSON.stringify(data),
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.text())
+        .then(result => responseFunction(JSON.parse(result)))
+        .catch(error => console.log('error', error));
+}
+
+const responseData = (result) => {
+    console.log("Server Response:", result, result.status);
+    if (result.status === true) {
+        Swal.fire({
+            icon: "success",
+            title: "Update Successful",
+            text: result.message,
+        }).then(() => {
+            window.location.href = "pemasukan.html";
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Update Failed",
+            text: result.message,
+        });
+    }
+}
+
+const updateTodo = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const _id = urlParams.get("_id");
+
+    console.log("todoID:", _id);
+
+    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/updatePemasukan?_id=" + _id;
+
+    const data = {
+        "tanggal_masuk": getValue("tanggal_masuk"),
+        "jumlah_masuk": getValue("jumlah_masuk"),
+        "sumber": getValue("sumber"),
+        "deskripsi" : getValue("deskripsi"),
+    };
+    
+    putData(target_url, data, responseData);
+
+    console.log("Data:", data);
+};
+
+const btnUpdates = document.getElementById("btnUpdate");
+
+btnUpdates.addEventListener("click", () => {
+    console.log("button aktif");
+    updateTodo();
+  });
