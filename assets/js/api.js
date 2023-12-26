9f689d658bbd73a2e293e44cbc182a20328c4d9b
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-26 20:39:33 +0700

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,21 +6,34 @@ export const getCookie = (name) => {
     if (parts.length === 2) return parts.pop().split(";").shift();
 };
 
-export const getWithToken = (target_url, responseFunction) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", getCookie("Authorization"));
+// api.js
 
-    const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+export const getWithToken = async (target_url) => {
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", getCookie("Authorization"));
 
-    fetch(target_url, requestOptions)
-        .then(response => response.text())
-        .then(result => responseFunction(JSON.parse(result)))
-        .catch(error => console.log('error', error));
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        const response = await fetch(target_url, requestOptions);
+        const result = await response.json();
+
+        if (response.ok) {
+            return { success: true, data: result };
+        } else {
+            console.error("Error in API response:", result);
+            return { success: false, error: result };
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return { success: false, error: error };
+    }
 };
+
 
 export const post = (target_url, datajson, responseFunction) => {
     const myHeaders = new Headers();
