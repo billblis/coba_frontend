9db72000af3ac91e205ef5111db0ec38af630b7b
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-26 19:24:21 +0700

    update

--- assets/js/getCard.js
+++ assets/js/getCard.js
@@ -1,23 +1,14 @@
-import {getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { formPemasukan } from "./table.js";
 import { formPengeluaran } from "./table.js";
 
 // Definisi fungsi getWithToken
-function getWithToken(target_url, responseFunction) {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", getCookie("Authorization"));
-
-    const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    fetch(target_url, requestOptions)
-        .then(response => response.text())
-        .then(result => responseFunction(JSON.parse(result)))
-        .catch(error => console.log('error', error));
+function getDataWithToken(targetUrl, responseFunction) {
+    getWithToken(targetUrl, (result) => {
+        if (result.status === true) {
+            responseFunction(result.data);
+        }
+    });
 }
 
 // Kode untuk pengeluaran
