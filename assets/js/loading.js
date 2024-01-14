086de667b85ef40ce718f7dcb7f0018af6bcd90b
Fedhira Syaila <pedirasyaila@gmail.com>
2024-01-14 11:06:18 +0700

    update

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,3 +1,7 @@
+// awesome-loading.js
+
+// import { showLoadingModal, hideLoadingModal } from './loading-modal';
+
 document.addEventListener("DOMContentLoaded", function () {
     const awesomeLoaderWrapper = document.createElement("div");
     awesomeLoaderWrapper.classList.add("awesome-loader-wrapper");
@@ -13,8 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
     awesomeLoaderWrapper.appendChild(awesomeLoaderContent);
     document.body.appendChild(awesomeLoaderWrapper);
 
-    const loadingModal = document.getElementById("loadingModal");
-
     function showAwesomeLoading() {
         document.body.style.overflow = "hidden"; // Prevent scrolling while loading
         awesomeLoaderWrapper.style.display = "flex";
@@ -25,26 +27,10 @@ document.addEventListener("DOMContentLoaded", function () {
         awesomeLoaderWrapper.style.display = "none";
     }
 
-    function showLoadingModal() {
-        loadingModal.style.display = "flex";
-    }
-
-    function hideLoadingModal() {
-        loadingModal.style.display = "none";
-    }
-
-    const loginButton = document.getElementById("button");
-
-    loginButton.addEventListener("click", function () {
-        showAwesomeLoading();
-        showLoadingModal();
-
-        // Simulate a login request (replace this with your actual login logic)
-        setTimeout(() => {
-            hideAwesomeLoading();
-            hideLoadingModal();
-            // Optionally, you can redirect to another page after successful login
-            window.location.href = "dashboard.html";
-        }, 3000);
-    });
+    showAwesomeLoading();
+    setTimeout(() => {
+        hideAwesomeLoading();
+        hideLoadingModal(); // Optionally hide the loading modal here
+    }, 3000);
 });
+
