c135d81f5caa234cc6bdd3272aca6eff1f53be74
Fedhira Syaila <pedirasyaila@gmail.com>
2024-01-20 21:28:05 +0700

    update

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function openProfileModal() {
-    document.getElementById('profileModal').style.display = 'block';
-  }
-  
-  function closeProfileModal() {
-    document.getElementById('profileModal').style.display = 'none';
-  }
-  
