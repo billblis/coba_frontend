a9083dc605a6ee7649b7a1ebeb100a0c5fb77c5c
Marlina Lubis <lubismarlina5@gmail.com>
2024-01-21 20:31:30 +0700

    update

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -28,15 +28,3 @@ document.addEventListener("DOMContentLoaded", function () {
     showAwesomeLoading();
     setTimeout(hideAwesomeLoading, 3000);
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-    const loaderWrapper = document.getElementById("loader-wrapper");
-    const contentWrapper = document.getElementById("app");
-  
-    contentWrapper.style.display = 'none';
-  
-    setTimeout(function () {
-      loaderWrapper.style.display = 'none';
-      contentWrapper.style.display = 'block';
-    }, 2000);
-  });
