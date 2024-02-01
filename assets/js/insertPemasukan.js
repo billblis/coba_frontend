d10181bde512a780d7925160592ed7d6e3490b08
Fedhira Syaila <pedirasyaila@gmail.com>
2024-02-01 20:08:40 +0700

    update

--- assets/js/insertPemasukan.js
+++ assets/js/insertPemasukan.js
@@ -3,19 +3,25 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js"
 
 const insertPemasukan = () => {
-    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan"
+    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan";
     const tokenkey = "Authorization";
     const tokenvalue = getCookie("Authorization");
+
+    // Get the current date in the format YYYY-MM-DD
+    const currentDate = new Date().toISOString().split('T')[0];
+
+    // Set the min attribute for the date input to the current date
+    document.getElementById("tanggal_masuk").setAttribute("min", currentDate);
 
     const data = {
         "tanggal_masuk": getValue("tanggal_masuk"),
         "jumlah_masuk": parseInt(getValue("jumlah_masuk")),
         "sumber": getValue("sumber"),
         "deskripsi": getValue("deskripsi"),
-    }
+    };
 
-    postWithToken(target_url, tokenkey, tokenvalue, data, responseData)
-}
+    postWithToken(target_url, tokenkey, tokenvalue, data, responseData);
+};
 
 const responseData = (result) => {
     if (result.status === true) {
@@ -33,8 +39,7 @@ const responseData = (result) => {
             text: result.message,
         });
     }
-}
+};
 
 const btnInsert = document.getElementById("btnInsert");
-
 btnInsert.addEventListener("click", insertPemasukan);
